c5622042639be03c6ad7c2aff8a148f2d1849851
The3Myst3ryX <villalobos_eduardo@yahoo.com>
2024-11-14 23:00:02 -0800

    Added the launcher icon

--- app/src/main/java/com/example/lab4_login/activites/SignInActivity.java
+++ app/src/main/java/com/example/lab4_login/activites/SignInActivity.java
@@ -64,9 +64,7 @@ public class SignInActivity extends AppCompatActivity {
                        preferenceManager.putString(Constants.KEY_NAME,documentSnapshot.getString(Constants.KEY_NAME));
                        preferenceManager.putString(Constants.KEY_IMAGE,documentSnapshot.getString(Constants.KEY_IMAGE));
 
-                       Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                       intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                       startActivity(intent);
+                       showToast("Successful Login");
                    }else{
                        loading(false);
                        showToast("Unable to Sign In");
